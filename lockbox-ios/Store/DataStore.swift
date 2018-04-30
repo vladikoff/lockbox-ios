f768aa2acd56945d3f57b68e8991adc323204a37
James Hugman <james@hugman.tv>
2018-04-30 14:36:53 +0200

    We have live data

--- lockbox-ios/Store/DataStore.swift
+++ lockbox-ios/Store/DataStore.swift
@@ -51,14 +51,6 @@ class DataStore {
         profile = BrowserProfile(localName: "lockbox-profile")
         FxALoginHelper.sharedInstance.application(UIApplication.shared, didLoadProfile: profile)
 
-        let state: SyncState
-        if !profile.hasSyncableAccount() {
-            state = .NotSyncable
-        } else {
-            state = .ReadyToSync
-        }
-
-        syncSubject.onNext(state)
         registerNotificationCenter()
 
         dispatcher.register
@@ -69,13 +61,24 @@ class DataStore {
                         self.onLogin(data)
                     case .sync:
                         self.onSync()
+                    case let .touch(id: id):
+                        self.onLoginUsage(id: id)
                     default: break
                     }
                 })
                 .disposed(by: self.disposeBag)
 
-        self.populateTestData()
         self.registerNotificationCenter()
+
+        let state: SyncState
+        if !profile.hasSyncableAccount() {
+            state = .NotSyncable
+        } else {
+            state = .ReadyToSync
+            updateList()
+        }
+
+        syncSubject.onNext(state)
     }
 
     deinit {
@@ -93,110 +96,15 @@ class DataStore {
 }
 
 extension DataStore {
-    public func populateTestData() {
-        let items = [
-            Item.Builder()
-                    .title("Amazon")
-                    .origins(["https://amazon.com"])
-                    .entry(ItemEntry.Builder()
-                            .kind("login")
-                            .username("tjacobson@example.com")
-                            .password("iLUVdawgz")
-                            .build())
-                    .build(),
-            Item.Builder()
-                    .title("Facebook")
-                    .origins(["https://www.facebook.com"])
-                    .entry(ItemEntry.Builder()
-                            .kind("login")
-                            .username("tanya.jacobson")
-                            .password("iLUVdawgz")
-                            .notes("I just have so much anxiety about using this website that I'm going to write about it in the notes section of my password manager wow") // swiftlint:disable:this line_length
-                            .build())
-                    .build(),
-            Item.Builder()
-                    .title("Reddit")
-                    .origins(["https://reddit.com"])
-                    .entry(ItemEntry.Builder()
-                            .kind("login")
-                            .username("tjacobson@example.com")
-                            .password("iLUVdawgz")
-                            .build())
-                    .build(),
-            Item.Builder()
-                    .title("Twitter")
-                    .origins(["http://www.twitter.com"])
-                    .entry(ItemEntry.Builder()
-                            .kind("login")
-                            .username("tjacobson@example.com")
-                            .password("iLUVdawgz")
-                            .build())
-                    .build(),
-            Item.Builder()
-                    .title("Blogspot")
-                    .origins(["https://accounts.google.com"])
-                    .entry(ItemEntry.Builder()
-                            .kind("login")
-                            .username("tjacobson@example.com")
-                            .password("iLUVdawgz")
-                            .notes("Meditation cloud bread cray, locavore actually chia everyday carry biodiesel venmo. Fashion axe polaroid seitan, put a bird on it stumptown selvage fam mustache thundercats viral. Squid tbh lo-fi celiac ennui occupy offal fam +1 disrupt ethical bushwick. Narwhal freegan man bun yr cray, heirloom try-hard mustache biodiesel. Direct trade tacos next level flexitarian tumeric cronut cornhole, brunch deep v tote bag brooklyn beard whatever gluten-free humblebrag. Cloud bread kale chips beard man braid, thundercats lo-fi forage chicharrones venmo four dollar toast lyft butcher echo park lumbersexual photo booth.") // swiftlint:disable:this line_length
-                            .build())
-                    .build(),
-            Item.Builder()
-                    .title("Chase")
-                    .origins(["https://www.chase.com"])
-                    .entry(ItemEntry.Builder()
-                            .kind("login")
-                            .username("jacobsonfamily444")
-                            .password("iLUVdawgz")
-                            .build())
-                    .build(),
-            Item.Builder()
-                    .title("Linkedin")
-                    .origins(["https://www.linkedin.com"])
-                    .entry(ItemEntry.Builder()
-                            .kind("login")
-                            .username("tanyamjackson@example.com")
-                            .password("iAmAprofessional345!")
-                            .build())
-                    .build(),
-            Item.Builder()
-                    .title("Bank of America")
-                    .origins(["http://www.bankofamerica.com"])
-                    .entry(ItemEntry.Builder()
-                            .kind("login")
-                            .username("tjacobson735")
-                            .password("iLUVdawgz")
-                            .build())
-                    .build(),
-            Item.Builder()
-                    .title("Comcast")
-                    .origins(["http://www.comcast.net"])
-                    .entry(ItemEntry.Builder()
-                            .kind("login")
-                            .username("tjacobsonlongfamilyusername@example.com")
-                            .password("iLUVdawgz")
-                            .build())
-                    .build(),
-            Item.Builder()
-                    .title("Zee Longest Title of a website you've ever seen")
-                    .origins(["www.verylongdomainthatmayormaynotexistintherealworld.com"])
-                    .entry(ItemEntry.Builder()
-                            .kind("login")
-                            .username("tjacobson")
-                            .password("veryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryveryverylongpassword") // swiftlint:disable:this line_length
-                            .build())
-                    .build()
-        ]
-
-//        self.listSubject.onNext(items)
+    public func onLogin(_ data: JSON) {
+        let helper = FxALoginHelper.sharedInstance
+        helper.application(UIApplication.shared, didReceiveAccountJSON: data)
     }
 }
 
 extension DataStore {
-    public func onLogin(_ data: JSON) {
-        let helper = FxALoginHelper.sharedInstance
-        helper.application(UIApplication.shared, didReceiveAccountJSON: data)
+    public func onLoginUsage(id: String) {
+        profile.logins.addUseOfLoginByGUID(id)
     }
 }
 
