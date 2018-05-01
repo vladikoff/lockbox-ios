3364da960793f824fdd9c71e0c80ab5183e91b1a
sashei <sashaheinen@gmail.com>
2018-05-01 15:46:01 +0200

    do not re-register client when logging in with fallback

--- lockbox-ios/Presenter/FxAPresenter.swift
+++ lockbox-ios/Presenter/FxAPresenter.swift
@@ -13,15 +13,20 @@ protocol FxAViewProtocol: class, ErrorView {
     func loadRequest(_ urlRequest: URLRequest)
 }
 
+struct LockedSyncState {
+    let locked: Bool
+    let state: SyncState
+}
+
 class FxAPresenter {
     private weak var view: FxAViewProtocol?
     fileprivate let fxAActionHandler: FxAActionHandler
     fileprivate let settingActionHandler: SettingActionHandler
     fileprivate let routeActionHandler: RouteActionHandler
     fileprivate let dataStoreActionHandler: DataStoreActionHandler
-    fileprivate let userInfoStore: UserInfoStore
     fileprivate let fxaStore: FxAStore
     fileprivate let dataStore: DataStore
+    fileprivate let userDefaults: UserDefaults
 
     private var disposeBag = DisposeBag()
 
@@ -36,17 +41,18 @@ class FxAPresenter {
          settingActionHandler: SettingActionHandler = SettingActionHandler.shared,
          routeActionHandler: RouteActionHandler = RouteActionHandler.shared,
          dataStoreActionHandler: DataStoreActionHandler = DataStoreActionHandler.shared,
-         userInfoStore: UserInfoStore = UserInfoStore.shared,
          fxaStore: FxAStore = FxAStore.shared,
-         dataStore: DataStore = DataStore.shared) {
+         dataStore: DataStore = DataStore.shared,
+         userDefaults: UserDefaults = UserDefaults.standard
+    ) {
         self.view = view
         self.fxAActionHandler = fxAActionHandler
         self.settingActionHandler = settingActionHandler
         self.routeActionHandler = routeActionHandler
         self.dataStoreActionHandler = dataStoreActionHandler
-        self.userInfoStore = userInfoStore
         self.fxaStore = fxaStore
         self.dataStore = dataStore
+        self.userDefaults = userDefaults
     }
 
     func onViewReady() {
@@ -86,12 +92,16 @@ class FxAPresenter {
 extension FxAPresenter {
     // The user has signed in to a Firefox Account.  We're done!
     func onLogin(_ data: JSON) {
-        self.dataStoreActionHandler.invoke(.initialize(blob: data))
-
-        self.dataStore.syncState.subscribe(onNext: { state in
-            if state == SyncState.Syncing {
-                self.routeActionHandler.invoke(MainRouteAction.list)
-            }
-        }).disposed(by: disposeBag)
+        Observable.combineLatest(self.userDefaults.onLock, self.dataStore.syncState)
+                .map { LockedSyncState(locked: $0.0, state: $0.1) }
+                .subscribe(onNext: { latest in
+                    if latest.locked {
+                        self.settingActionHandler.invoke(.visualLock(locked: false))
+                        self.routeActionHandler.invoke(MainRouteAction.list)
+                    } else if latest.state == SyncState.Syncing {
+                        self.dataStoreActionHandler.invoke(.initialize(blob: data))
+                    }
+                })
+                .disposed(by: self.disposeBag)
     }
 }
